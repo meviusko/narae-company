da716d655263bf68a9d3f977f35cf999f3faca5b
meviusko <gereltod1993@gmail.com>
2025-04-30 19:32:19 +0900

    Q

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-import { getData } from './js/firebase-config.js';
-import { ref, get } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
-import { db } from './js/firebase-config.js';
+// Firebase SDK import
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-analytics.js";
+import { getDatabase, ref, get } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-database.js";
+
+// Firebase 설정
+const firebaseConfig = {
+  apiKey: "AIzaSyB00VqMZrAMXtD5K1gH6P-cvj36Hl0ibus",
+  authDomain: "nrcopany.firebaseapp.com",
+  databaseURL: "https://nrcopany-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "nrcopany",
+  storageBucket: "nrcopany.firebasestorage.app",
+  messagingSenderId: "869162796484",
+  appId: "1:869162796484:web:0cc702d188090aeedc1091",
+  measurementId: "G-105X4NDLJ8"
+};
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase(app);
 
 // 전역 변수
 let currentSlide = 0;
